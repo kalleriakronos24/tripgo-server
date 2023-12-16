ba15d455d955b5ebe1c69cd808d03f58b797a2a1
kalleriakronos24 <kalleriakronos24@gmail.com>
2023-12-16 18:07:44 +0800

    fix: cors allow all origin 0.1

--- controllers/middlewares/cors.go
+++ controllers/middlewares/cors.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware(c *gin.Context) gin.HandlerFunc {
+func CORSMiddleware(c *gin.Context) {
 	//cors.New(cors.Config{
 	//	AllowOrigins:     []string{"*"},
 	//	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
@@ -16,17 +16,15 @@ func CORSMiddleware(c *gin.Context) gin.HandlerFunc {
 	//})
 	//c.Next()
 
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
 	}
+
+	c.Next()
 }
