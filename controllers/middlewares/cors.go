1aec85171b4afb481b9ea62da52bfbba5aab1a15
kalleriakronos24 <kalleriakronos24@gmail.com>
2023-12-16 17:59:05 +0800

    fix: cors allow all origin

--- controllers/middlewares/cors.go
+++ controllers/middlewares/cors.go
@@ -1,21 +1,32 @@
 package middleware
 
 import (
-	"time"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware(c *gin.Context) {
-	cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
-		AllowHeaders:     []string{"Content-Type", "Content-Length", "Authorization", "Origin"},
-		ExposeHeaders:    []string{"Content-Type", "Content-Length"},
-		AllowCredentials: true,
-		AllowWebSockets:  true,
-		MaxAge:           12 * time.Hour,
-	})
-	c.Next()
+func CORSMiddleware(c *gin.Context) gin.HandlerFunc {
+	//cors.New(cors.Config{
+	//	AllowOrigins:     []string{"*"},
+	//	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+	//	AllowHeaders:     []string{"Content-Type", "Content-Length", "Authorization", "Origin"},
+	//	ExposeHeaders:    []string{"Content-Type", "Content-Length"},
+	//	AllowCredentials: true,
+	//	AllowWebSockets:  true,
+	//	MaxAge:           12 * time.Hour,
+	//})
+	//c.Next()
+
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
