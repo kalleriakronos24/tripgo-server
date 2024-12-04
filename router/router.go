8978aa84528e3407fbad899b48f8246615f5d8df
madanugraha <mada@globalhealthpress.org>
2024-12-04 15:35:14 +0800

    fix: cors localhost

--- router/router.go
+++ router/router.go
@@ -40,7 +40,7 @@ func InitializeRouter() (router *gin.Engine) {
 	str := []string{"http://localhost:4321", "http://localhost:3000"}
 
 	if config.AppConfig.Environment == "PRODUCTION" {
-		str = []string{"https://wadahgo.com"}
+		str = []string{"https://wadahgo.com", "http://localhost:3000"}
 	}
 
 	configCors := cors.DefaultConfig()
