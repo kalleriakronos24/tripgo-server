f9bd3d65dd630f73a238692eba2a2ed1b8392aed
kalleriakronos24 <madacool22@gmail.com>
2025-01-20 19:35:30 +0800

    fix: find order logic

--- services/booking_transfer.go
+++ services/booking_transfer.go
@@ -46,29 +46,40 @@ func (module *module) InsertBookingTransfer(p *dto.InsertBookingTransfer) (err e
 
 	tx := database.GetDatabaseConnection().Begin()
 
-	var driverBalance *models.BalanceDriver
-	if driverBalance, err = module.db.balanceDriver.GetAllDriverHasEnoughBalance(float64(p.Price), p.CarModelID); err != nil {
-		tx.Rollback()
-		return errors.New("drivers seems busy. please try again later")
-	}
-
-	var driverBalanceDetail *models.BalanceDriver
-	if driverBalanceDetail, err = module.db.balanceDriver.GetOneDetailByID(driverBalance.ID); err != nil {
-		tx.Rollback()
-		return errors.New("drivers seems busy. please try again later")
-	}
-
+	// TODO CHANGE TO NEAREST LOCATION FROM COMPANY ADDRESS
 	var carManagement master.CarManagement
 	if _, err = module.db.carManagementModel.CheckKhaimalDriverAvailable(p.CarModelID); err == nil {
 		if carManagement, err = module.db.carManagementModel.GetKhaimalManagerId(p.CarModelID); err == nil {
 			log.Println("THIS1")
 		} else {
+			var driverBalance *models.BalanceDriver
+			if driverBalance, err = module.db.balanceDriver.GetAllDriverHasEnoughBalance(float64(p.Price), p.CarModelID); err != nil {
+				tx.Rollback()
+				return errors.New("drivers seems busy. please try again later")
+			}
+
+			var driverBalanceDetail *models.BalanceDriver
+			if driverBalanceDetail, err = module.db.balanceDriver.GetOneDetailByID(driverBalance.ID); err != nil {
+				tx.Rollback()
+				return errors.New("drivers seems busy. please try again later")
+			}
 			if carManagement, err = module.db.carManagementModel.GetOneByCarModelIDAndAvailable(p.CarModelID, driverBalanceDetail.Driver.ID, driverBalanceDetail.Driver.Company.ID); err != nil {
 				tx.Rollback()
 				return errors.New("drivers seems busy. please try again later")
 			}
 		}
 	} else {
+		var driverBalance *models.BalanceDriver
+		if driverBalance, err = module.db.balanceDriver.GetAllDriverHasEnoughBalance(float64(p.Price), p.CarModelID); err != nil {
+			tx.Rollback()
+			return errors.New("drivers seems busy. please try again later")
+		}
+
+		var driverBalanceDetail *models.BalanceDriver
+		if driverBalanceDetail, err = module.db.balanceDriver.GetOneDetailByID(driverBalance.ID); err != nil {
+			tx.Rollback()
+			return errors.New("drivers seems busy. please try again later")
+		}
 		if carManagement, err = module.db.carManagementModel.GetOneByCarModelIDAndAvailable(p.CarModelID, driverBalanceDetail.Driver.ID, driverBalanceDetail.Driver.Company.ID); err != nil {
 			tx.Rollback()
 			return errors.New("drivers seems busy. please try again later")
@@ -119,14 +130,14 @@ func (module *module) InsertBookingTransfer(p *dto.InsertBookingTransfer) (err e
 
 	formattedNotificationMessage := fmt.Sprintf("Booking Transfer Request <br/> %v <br/> %v Person Pax x %v Luggagge <br/> Pickup Date %v <br/> Notes: %v <br/> Price: %v", carManagement.Name, carManagement.CarModel.PersonCount, carManagement.CarModel.LuggageCount, utils.ConvertEnToIDDateTime(bookingTransfer.PickUpDate), bookingTransfer.PassengerNotes, bookingTransfer.GrandTotal)
 	// send notification to the selected driver
-	if err = onesignal.PushNotificationSingleExternalId(driverBalanceDetail.Driver.Credentials.Email, formattedNotificationMessage); err != nil {
+	if err = onesignal.PushNotificationSingleExternalId(carManagement.Driver.Credentials.Email, formattedNotificationMessage); err != nil {
 		return errors.New("server-error. please try again later")
 	}
 
 	// send backup email to the driver
 	if err = mail.SendMailV3(&mail.TSendMail{
 		From:    "WadahGo <notification@wadahgo.com>",
-		MailTo:  driverBalanceDetail.Driver.Credentials.Email,
+		MailTo:  carManagement.Driver.Credentials.Email,
 		Subject: "Booking Transfer Received",
 		Body: fmt.Sprintf(`<html><body>
 		<p>%v</p>
