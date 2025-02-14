52d420629dbb9aa0425556a3961cf14530599118
kalleriakronos24 <madacool22@gmail.com>
2025-02-14 15:48:11 +0800

    fix: payment intent

--- controllers/v1/booking-transfer.go
+++ controllers/v1/booking-transfer.go
@@ -63,7 +63,7 @@ func POSTBookingTransfer(c *gin.Context) {
 		TotalDistance:     pValidator.TotalDistance,
 		RefferalCode:      pValidator.RefferalCode,
 		PaymentOption:     pValidator.PaymentOption,
-		PI:                "TEST",
+		PI:                pValidator.PI,
 	}
 
 	if err = services.Handler.InsertBookingTransfer(p); err != nil {
